649217ff403ffeba8d39f969d07929bffd85b12a
Ricky Elrod <ricky@elrod.me>
2015-12-17 19:35:49 -0500

    Start of an attoparsec-based FEN parser.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ src/Game/Chess/FEN.hs
@@ -0,0 +1,38 @@
+-- | A simple FEN parser using attoparsec.
+module Game.Chess.FEN where
+
+import Data.Attoparsec.Text
+import Data.Char (digitToInt, isDigit, isLower, isUpper, toLower)
+import Data.List (elem, intersperse)
+import Game.Chess.Types
+
+charToCells :: Char -> [Cell]
+charToCells c
+  | isDigit c = replicate (digitToInt c) Empty
+  | isLower c = [cellify (toLower c) Black]
+  | isUpper c = [cellify (toLower c) White]
+  | otherwise = []
+  where
+    cellify 'p' = Cell Pawn
+    cellify 'r' = Cell Rook
+    cellify 'n' = Cell Knight
+    cellify 'k' = Cell King
+    cellify 'q' = Cell Queen
+    cellify 'b' = Cell Bishop
+
+parseCell :: Parser Char
+parseCell =
+  choice $ map char "12345678" ++ map char "PNBRQKpnbrqk"
+
+parseRanks :: Parser [Cell]
+parseRanks = do
+  ranks <- many1 parseCell `sepBy1` (char '/')   -- [String]
+  let parsedRanks = map (concatMap charToCells) ranks
+  return . concat . intersperse (replicate 8 Empty) $ parsedRanks
+
+parseActiveColor :: Parser Color
+parseActiveColor = do
+  color <- choice [char 'b', char 'w']
+  if color == 'b'
+    then return Black
+    else return White
