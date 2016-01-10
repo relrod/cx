c48d33fd0e8c3458dba1c176ef9748b5c4625d0d
Ricky Elrod <ricky@elrod.me>
2016-01-10 00:20:14 -0500

    Move generation!!!!!!
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Game/Chess/Position.hs
+++ src/Game/Chess/Position.hs
@@ -1,6 +1,9 @@
 module Game.Chess.Position where
 
+import Data.Maybe (catMaybes)
 import qualified Data.Vector as V
+import Game.Chess.Board (index, move)
+import Game.Chess.Piece (movingVectors)
 import Game.Chess.Types
 
 -- | Given a position vector in the form (file, rank), add it to a given
@@ -9,3 +12,14 @@ movePosition :: (Int, Int) -> Position -> Maybe Position
 movePosition (x, y) (Position (PFile f) (PRank r)) =
   mkPosition (mkFile (f + x)) (mkRank (r + y))
 {-# INLINE movePosition #-}
+
+-- | Move generation!
+generate :: Board -> Position -> [Board]
+generate brd pos =
+  case index brd pos of
+    Empty -> []
+    Cell piece color -> catMaybes $ fmap doMovePosition (movingVectors piece)
+  where
+    doMovePosition vect = do
+      newPos <- movePosition vect pos
+      return (move pos newPos brd)
